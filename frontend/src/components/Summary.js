abb0ad0f716a5f283c9fef2b163d656594d4903d
Xenans <xenansgaming@gmail.com>
2022-02-20 01:54:10 -0800

    wip

--- frontend/src/components/Summary.js
+++ frontend/src/components/Summary.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 
 import Button from 'react-bootstrap/Button';
+import CloseButton from 'react-bootstrap/CloseButton';
+import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
 import { Link } from 'react-router-dom'
 
 class Summary extends Component {
@@ -12,10 +15,48 @@ class Summary extends Component {
 
   render() {
       return (
-          <h1>Add a Person:</h1>
+          <SummaryListItem item={{name:"hello", price:"123"}}/> 
       );
   }
 }
+
+
+class SummaryList extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1></h1>
+        )
+    }
+}
+
+class SummaryListItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Form.Group className="w-25 p-3">
+                <Row>
+                    <Col>
+                        <Form.Control
+                            type="text" readOnly 
+                            value={this.props.item.name}
+                        />
+                        <Form.Control
+                            type="number" readOnly 
+                            value={this.props.item.price}
+                        />
+                    </Col>
+                    {/* <CloseButton onClick={() => this.props.onDelete(this.props.item.id)} /> */}
+                </Row>
+            </Form.Group>
+        )
+    }
+}
+
 
 
 export default Summary
