28a553104d7e58fd063a2c7867d9b4cc5fc46431
Xenans <xenansgaming@gmail.com>
2022-02-19 18:37:38 -0800

    Separated pages for routing

--- frontend/src/components/FoodSelection.js
+++ frontend/src/components/FoodSelection.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom'
 
 import '../style/FoodSelection.css';
 
@@ -15,7 +16,8 @@ class Header extends Component {
             <>
                 {this.props.items.map((item) =>
                     (<h1 onClick={() => this.props.onClick(item.itemName, this)}>{item.itemName}: ${item.itemPrice}</h1>))}
-                <Button className="nextButton">Next</Button>
+                {/* <Button className="nextButton">Next</Button> */}
+                <Link to="/additems" className="nextButton">Next</Link>
             </>
         );
     }
